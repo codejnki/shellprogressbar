d8505807f950742e63cbc1ecff3f4dc22f1b7915
Patrick Stockton <patrick@codejnki.com>
2015-11-06 21:05:43 -0500

    Fixes for compatiblity on a Mac using mono

--- src/ShellProgressBar.Example/Program.cs
+++ src/ShellProgressBar.Example/Program.cs
@@ -7,65 +7,68 @@ using System.Threading.Tasks;
 
 namespace ShellProgressBar.Example
 {
-	class Program
-	{
-		static void Main(string[] args)
-		{
-			var ticks = 10;
-			using (var pbar = new ProgressBar(ticks, "A console progress bar that never ticks"))
-			{
-			}
-			using (var pbar = new ProgressBar(ticks, "A console progress bar does not complete"))
-			{
-				pbar.Tick();
-				pbar.Tick();
-				pbar.Tick();
-				pbar.Tick();
-			}
-			ticks = 100;
-			using (var pbar = new ProgressBar(ticks, "my long running operation", ConsoleColor.Green))
-			{
-				for (var i = 0; i < ticks; i++)
-				{
-					pbar.Tick("step " + i);
-					Thread.Sleep(50);
-				}
-			}
-			ticks = 0;
-			using (var pbar = new ProgressBar(ticks, "my operation with zero ticks", ConsoleColor.Cyan))
-			{
-				for (var i = 0; i < ticks; i++)
-				{
-					pbar.Tick("step " + i);
-					Thread.Sleep(50);
-				}
-			}
-			ticks = -100;
-			using (var pbar = new ProgressBar(ticks, "my operation with negative ticks", ConsoleColor.Cyan))
-			{
-				for (var i = 0; i < ticks; i++)
-				{
-					pbar.Tick("step " + i);
-					Thread.Sleep(50);
-				}
-			}
-			ticks = 10;
-			using (var pbar = new ProgressBar(ticks, "My operation that ticks to often", ConsoleColor.Cyan))
-			{
-				for (var i = 0; i < ticks * 10; i++)
-				{
-					pbar.Tick("too many steps " + i);
-					Thread.Sleep(50);
-				}
-			}
-			ticks = 200;
-			using (var pbar = new ProgressBar(ticks / 10, "My operation that ticks to often using threads", ConsoleColor.Cyan))
-			{
-				var threads = Enumerable.Range(0, ticks).Select(i => new Thread(() => pbar.Tick("threaded tick " + i))).ToList();
-				foreach (var thread in threads) thread.Start();
-				foreach (var thread in threads) thread.Join();
-			}
-			Console.ReadLine();
-		}
-	}
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.Clear();
+      var ticks = 10;
+      using (var pbar = new ProgressBar(ticks, "A console progress bar that never ticks"))
+      {
+      }
+      using (var pbar = new ProgressBar(ticks, "A console progress bar does not complete"))
+      {
+        pbar.Tick();
+        pbar.Tick();
+        pbar.Tick();
+        pbar.Tick();
+      }
+      ticks = 100;
+      using (var pbar = new ProgressBar(ticks, "my long running operation", ConsoleColor.Green))
+      {
+        for (var i = 0; i < ticks; i++)
+        {
+          pbar.Tick("step " + i);
+          Thread.Sleep(50);
+        }
+      }
+      ticks = 0;
+      using (var pbar = new ProgressBar(ticks, "my operation with zero ticks", ConsoleColor.Cyan))
+      {
+        for (var i = 0; i < ticks; i++)
+        {
+          pbar.Tick("step " + i);
+          Thread.Sleep(50);
+        }
+      }
+      ticks = -100;
+      using (var pbar = new ProgressBar(ticks, "my operation with negative ticks", ConsoleColor.Cyan))
+      {
+        for (var i = 0; i < ticks; i++)
+        {
+          pbar.Tick("step " + i);
+          Thread.Sleep(50);
+        }
+      }
+      ticks = 10;
+      using (var pbar = new ProgressBar(ticks, "My operation that ticks to often", ConsoleColor.Cyan))
+      {
+        for (var i = 0; i < ticks * 10; i++)
+        {
+          pbar.Tick("too many steps " + i);
+          Thread.Sleep(50);
+        }
+      }
+      ticks = 200;
+      using (var pbar = new ProgressBar(ticks / 10, "My operation that ticks to often using threads", ConsoleColor.Cyan))
+      {
+        var threads = Enumerable.Range(0, ticks).Select(i => new Thread(() => pbar.Tick("threaded tick " + i))).ToList();
+        foreach (var thread in threads)
+          thread.Start();
+        foreach (var thread in threads)
+          thread.Join();
+      }
+      Console.ReadLine();
+    }
+  }
 }
--- src/ShellProgressBar/ProgressBar.cs
+++ src/ShellProgressBar/ProgressBar.cs
@@ -3,117 +3,125 @@ using System.Threading;
 
 namespace ShellProgressBar
 {
-    public class ProgressBar : IDisposable
+  public class ProgressBar : IDisposable
+  {
+    private static readonly object _lock = new object();
+
+    private readonly int _maxTicks;
+    private readonly ConsoleColor _color;
+    private readonly DateTime _startDate = DateTime.Now;
+
+    private int _currentTick = 0;
+    private string _message = null;
+    private Timer _timer = null;
+    private readonly char _progressCharacter;
+
+    private static int ConsoleWidth { get { return Console.LargestWindowWidth - 1; } }
+
+    private static int ConsoleHeight { get { return Console.LargestWindowHeight - 1; } }
+
+    public int CurrentTick { get { return Math.Min(_maxTicks, _currentTick); } }
+
+    public ProgressBar(int maxTicks, string message, ConsoleColor color = ConsoleColor.Green, char progressCharacter = '\u2588')
     {
-        private static readonly object _lock = new object();
-
-        private readonly int _maxTicks;
-        private readonly ConsoleColor _color;
-        private readonly DateTime _startDate = DateTime.Now;
-
-        private int _currentTick = 0;
-        private string _message = null;
-        private Timer _timer = null;
-        private readonly char _progressCharacter;
-
-		public int CurrentTick { get { return Math.Min(_maxTicks, _currentTick); } }
-
-        public ProgressBar(int maxTicks, string message, ConsoleColor color = ConsoleColor.Green, char progressCharacter = '\u2588')
-        {
-            _progressCharacter = progressCharacter;
-            _maxTicks = Math.Max(0, maxTicks);
-            _message = message;
-            _color = color;
-            Console.WriteLine();
-            _timer = new Timer((s) => DisplayProgress(), null, 500, 500);
-            DisplayProgress();
-        }
-
-        public void Tick(string message = "")
-        {
-            Interlocked.Increment(ref _currentTick);
-            if (message != "")
-                Interlocked.Exchange(ref _message, message);
-
-            DisplayProgress();
-
-        }
-
-        public void Message(string message)
-        {
-            if (message != "") Interlocked.Exchange(ref _message, message);
-
-            DisplayProgress();
-        }
-
-        private void DisplayProgress()
-        {
-            if (_timer == null) return;
-
-            double percentage = Math.Max(0, Math.Min(100, (100.0 / _maxTicks) * this.CurrentTick));
-            // Gracefully handle if the percentage is NaN due to division by 0
-            if (Double.IsNaN(percentage) || percentage < 0) percentage = 100;
-            var duration = (DateTime.Now - _startDate);
-            var durationString = string.Format("{0:00}:{1:00}:{2:00}", duration.Hours, duration.Minutes, duration.Seconds);
-            var column1width = Console.WindowWidth - durationString.Length - 2;
-            var column2width = durationString.Length;
-            var format = string.Format("{{0, -{0}}} {{1,{1}}}", column1width, column2width);
-
-            var message = StringExtensions.Excerpt(string.Format("{0:N2}%", percentage) + " " + _message, column1width);
-            var formatted = String.Format(format, message, durationString);
-            lock (_lock)
-            {
-                RenderConsoleProgress(percentage, _progressCharacter, _color, formatted);
-                if (percentage > 100)
-                {
-                    _timer.Dispose();
-                    _timer = null;
-                }
-            }
-
-        }
-
-
-        public static void OverwriteConsoleMessage(string message)
-        {
-            Console.CursorLeft = 0;
-            int maxCharacterWidth = Console.WindowWidth - 1;
-            message = message + new string(' ', maxCharacterWidth - message.Length);
-            Console.Write(message);
-        }
-
-        public static void RenderConsoleProgress(double percentage, char progressBarCharacter, ConsoleColor color, string message)
-        {
-            Console.CursorVisible = false;
-            ConsoleColor originalColor = Console.ForegroundColor;
-            Console.ForegroundColor = color;
-            Console.CursorLeft = 0;
-			var currentTop = Console.CursorTop;
-
-            int width = Console.WindowWidth - 1;
-            int newWidth = (int)((width * percentage) / 100d);
-            string progBar = new string(progressBarCharacter, newWidth) + new string(' ', width - newWidth);
-            Console.Write(progBar);
-
-            if (string.IsNullOrEmpty(message)) message = "";
-            //Console.CursorTop = Math.Min(Console.BufferHeight - 1, Console.CursorTop + 1);
-			Console.CursorTop = currentTop + 1;
-            OverwriteConsoleMessage(message);
-			Console.CursorTop = currentTop;
-
-            Console.ForegroundColor = originalColor;
-            Console.CursorVisible = true;
-        }
-
-
-        public void Dispose()
-        {
-            Console.WriteLine();
-            if (_timer != null)
-                _timer.Dispose();
-            _timer = null;
-			Console.WriteLine();
-        }
+      _progressCharacter = progressCharacter;
+      _maxTicks = Math.Max(0, maxTicks);
+      _message = message;
+      _color = color;
+      Console.WriteLine();
+      _timer = new Timer((s) => DisplayProgress(), null, 500, 500);
+      DisplayProgress();
     }
+
+    public void Tick(string message = "")
+    {
+      Interlocked.Increment(ref _currentTick);
+      if (message != "")
+        Interlocked.Exchange(ref _message, message);
+
+      DisplayProgress();
+
+    }
+
+    public void Message(string message)
+    {
+      if (message != "")
+        Interlocked.Exchange(ref _message, message);
+
+      DisplayProgress();
+    }
+
+    private void DisplayProgress()
+    {
+      if (_timer == null)
+        return;
+
+      double percentage = Math.Max(0, Math.Min(100, (100.0 / _maxTicks) * this.CurrentTick));
+      // Gracefully handle if the percentage is NaN due to division by 0
+      if (Double.IsNaN(percentage) || percentage < 0)
+        percentage = 100;
+      var duration = (DateTime.Now - _startDate);
+      var durationString = string.Format("{0:00}:{1:00}:{2:00}", duration.Hours, duration.Minutes, duration.Seconds);
+      var column1width = ConsoleWidth == 0 ? 60 : ConsoleWidth - durationString.Length - 2;
+      var column2width = durationString.Length;
+      var format = string.Format("{{0, -{0}}} {{1,{1}}}", column1width, column2width);
+
+      var message = StringExtensions.Excerpt(string.Format("{0:N2}%", percentage) + " " + _message, column1width);
+      var formatted = String.Format(format, message, durationString);
+      lock (_lock)
+      {
+        RenderConsoleProgress(percentage, _progressCharacter, _color, formatted);
+        if (percentage > 100)
+        {
+          _timer.Dispose();
+          _timer = null;
+        }
+      }
+
+    }
+
+
+    public static void OverwriteConsoleMessage(string message)
+    {
+      Console.CursorLeft = 0;
+      int maxCharacterWidth = ConsoleWidth;
+      message = message + new string(' ', maxCharacterWidth - message.Length > 0 ? maxCharacterWidth - message.Length : 0);
+      Console.Write(message);
+    }
+
+    public static void RenderConsoleProgress(double percentage, char progressBarCharacter, ConsoleColor color, string message)
+    {
+      Console.CursorVisible = false;
+      ConsoleColor originalColor = Console.ForegroundColor;
+      Console.ForegroundColor = color;
+      Console.CursorLeft = 0;
+      var currentTop = Console.CursorTop;
+
+      int width = ConsoleWidth - 2;
+      int newWidth = (int)((width * percentage) / 100d);
+      string progBar = new string(progressBarCharacter, newWidth) + new string(' ', width - newWidth);
+      Console.Write(progBar);
+
+      if (string.IsNullOrEmpty(message))
+        message = "";
+      //Console.CursorTop = Math.Min(Console.BufferHeight - 1, Console.CursorTop + 1);
+      Console.CursorTop = currentTop + 1;
+      OverwriteConsoleMessage(message);
+      Console.CursorTop = currentTop;
+
+      Console.ForegroundColor = originalColor;
+      Console.CursorVisible = true;
+    }
+
+
+    public void Dispose()
+    {
+      Console.WriteLine();
+      if (_timer != null)
+        _timer.Dispose();
+      _timer = null;
+      Console.WriteLine();
+    }
+  }
 
 }
